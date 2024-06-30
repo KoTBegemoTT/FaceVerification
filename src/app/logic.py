d30e2da4b08fc8dfc5171a080fc6e516cff4ccb7
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-06-30 19:04:18 +0000

    Исправил с учётом правил линтера

--- src/app/logic.py
+++ src/app/logic.py
@@ -2,10 +2,12 @@ from deepface import DeepFace
 
 
 class FaceVerification:
-    @staticmethod
-    def image_to_vector(path: str):
-        '''Функция превращает изображение в вектор.'''
+    """Класс предоставляющий возможности для верификации изображений."""
+
+    @classmethod
+    def image_to_vector(cls, path: str):
+        """Функция превращает изображение в вектор."""
         embedding_objs = DeepFace.represent(
-            img_path=path, model_name="Facenet", enforce_detection=False
+            img_path=path, model_name='Facenet', enforce_detection=False,
         )
-        return embedding_objs[0]["embedding"]
+        return embedding_objs[0]['embedding']
