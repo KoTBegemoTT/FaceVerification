8d1af6cffb7395a85217e21f59b0732a540a45af
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-07-03 18:45:18 +0000

    Добавлено новое изображение для тестирования и обновлены тесты, с помощью pytest

--- src/tests/test_logic.py
+++ src/tests/test_logic.py
@@ -1,6 +1,27 @@
+import pytest
 from app.logic import FaceVerification
 
-def test_image_to_vetor_convert():
-    embedding = FaceVerification.image_to_vector("src/tests/images/image_for_vectorise.jpeg")
-    assert type(embedding) == list
-    assert len(embedding) == 128
+
+class TestFaceVerification:
+    @pytest.mark.parametrize(
+        "path",
+        [
+            "src/tests/images/image_for_vectorise.jpeg",
+            "src/tests/images/image_for_vectorise2.png",
+        ]
+    )
+    def test_image_to_vetor_convert(self, path):
+        embedding = FaceVerification.image_to_vector(path)
+        assert type(embedding) == list
+        assert len(embedding) == 128
+
+    @pytest.mark.parametrize(
+        "path, error",
+        [
+            ("not_exist.jpg", ValueError),
+            (10001, ValueError),
+        ]
+    )
+    def test_image_to_vetor_fail(self, path, error):
+        with pytest.raises(error):
+            embedding = FaceVerification.image_to_vector(path)
