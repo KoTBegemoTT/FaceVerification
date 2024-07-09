c5737eba87992d76906ba42caaacc34f40c44246
LebedevPavel2 <lebedevpavel2@yandex.ru>
2024-07-09 08:59:25 +0000

    Добавлено id для праметров тестов

--- src/tests/test_logic.py
+++ src/tests/test_logic.py
@@ -7,8 +7,14 @@ class TestFaceVerification:
     @pytest.mark.parametrize(
         'path',
         [
-            'src/tests/images/image_for_vectorise.jpeg',
-            'src/tests/images/image_for_vectorise2.png',
+            pytest.param(
+                'src/tests/images/image_for_vectorise.jpeg',
+                id='first_image',
+            ),
+            pytest.param(
+                'src/tests/images/image_for_vectorise_2.png',
+                id='second_image',
+            ),
         ],
     )
     def test_image_to_vetor_convert(self, path):
@@ -20,8 +26,8 @@ class TestFaceVerification:
     @pytest.mark.parametrize(
         'path, error',
         [
-            ('not_exist.jpg', ValueError),
-            (10001, ValueError),
+            pytest.param('not_exist.jpg', ValueError, id='not_exist'),
+            pytest.param(10001, ValueError, id='wrong_path_type'),
         ],
     )
     def test_image_to_vetor_fail(self, path, error):
